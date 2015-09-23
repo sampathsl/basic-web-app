e87fdcede881e445ac8fc73b6273a4477d4de05e
sampathSL <sampath.thennakoon@gmail.com>
2015-09-23 15:24:53 +0530

    Add some changes

--- src/main/java/com/tutorial/entity/BlogEntry.java
+++ src/main/java/com/tutorial/entity/BlogEntry.java
@@ -1,8 +1,12 @@
 package com.tutorial.entity;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by test on 9/23/2015.
  */
+
+@Service
 public class BlogEntry {
     private String title;
 
--- src/main/java/com/tutorial/mvc/BlogEntryController.java
+++ src/main/java/com/tutorial/mvc/BlogEntryController.java
@@ -1,11 +1,14 @@
 package com.tutorial.mvc;
 
 import com.tutorial.entity.BlogEntry;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by sampath on 9/22/2015.
@@ -15,17 +18,35 @@ public class BlogEntryController {
 
     /*@Autowired
     BlogEntry blogEntry;*/
+	
+	private BlogEntry blogEntry = null;
 
-    @RequestMapping("/test1")
+	@Autowired
+	public BlogEntryController(BlogEntry blogEntry) {
+		this.blogEntry = blogEntry;
+	}
+
+   @RequestMapping("/test")
     public String showViewJSP(){
         return "view";
     }
 
-    @RequestMapping("/test")
+   @RequestMapping("/blog-entry")
     public ResponseEntity<Object> showResponseEntity(){
         BlogEntry blogEntry = new BlogEntry();
         blogEntry.setTitle("Test Blog Entry");
         return new ResponseEntity<Object>(blogEntry,HttpStatus.OK);
     }
+   
+   @RequestMapping(value = "/todos", method = RequestMethod.GET)
+	public ModelAndView hello() {
+
+		ModelAndView model = new ModelAndView();
+		model.setViewName("index");
+		blogEntry.setTitle("TITLE - INDEX WORK");
+		model.addObject("title",blogEntry.getTitle());
+		
+		return model;
+	}
 
 }
